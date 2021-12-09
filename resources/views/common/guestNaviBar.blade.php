5914e88903ac624685f57c97df608dc370ba85d7
chathuraHarshanga <chathura749@gmail.com>
2021-12-09 22:33:38 +0530

    navi bar

--- resources/views/common/guestNaviBar.blade.php
+++ resources/views/common/guestNaviBar.blade.php
@@ -4,4 +4,6 @@
   <li><a href="#news">News</a></li>
   <li><a href="#contact">Contact</a></li>
   <li><a href="#about">About</a></li>
+  <li><a href="#about">Lgin</a></li>
+  <li><a href="#about">Sign up</a></li>
 </ul>
